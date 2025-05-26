4b3535012728c99a233fc6e330248cd60b622c39
kien.nguyen <kien.nguyen@asnet.com.vn>
2025-05-26 11:16:11 +0700

    feat: change data

--- scripts/generate-products.js
+++ scripts/generate-products.js
@@ -1,5 +1,5 @@
-const { faker } = require('@faker-js/faker');
-const fs = require('fs');
+const { faker } = require("@faker-js/faker");
+const fs = require("fs");
 
 const products = Array.from({ length: 1000 }, (_, index) => ({
   name: `${faker.commerce.productName()} ${index + 1}`,
@@ -8,16 +8,23 @@ const products = Array.from({ length: 1000 }, (_, index) => ({
   category: faker.commerce.department(),
   rating: faker.number.int({ min: 1, max: 5 }),
   reviewCount: faker.number.int({ min: 0, max: 500 }),
-  images: [
-    `https://example.com/images/product${index + 1}-1.jpg`,
-    `https://example.com/images/product${index + 1}-2.jpg`
-  ],
-  image: `https://example.com/images/product${index + 1}-main.jpg`,
+  images: [],
+  image: `https://m.media-amazon.com/images/I/61j3e6uWiuL._AC_SL1500_.jpg`,
   imageList: [
-    `https://example.com/images/product${index + 1}-3.jpg`,
-    `https://example.com/images/product${index + 1}-4.jpg`
-  ]
+    {
+      color: "#FFFFFF",
+      image: "https://m.media-amazon.com/images/I/61j3e6uWiuL._AC_SL1500_.jpg",
+    },
+    {
+      color: "#B4916C",
+      image: "https://m.media-amazon.com/images/I/61lJK9kXTqL._AC_SL1500_.jpg",
+    },
+    {
+      color: "#E4CBAD",
+      image: "https://m.media-amazon.com/images/I/71a2eOmVxLL._AC_SL1500_.jpg",
+    },
+  ],
 }));
 
-fs.writeFileSync('../products.json', JSON.stringify(products, null, 2));
-console.log('Generated products.json with 1000 items');
+fs.writeFileSync("../products.json", JSON.stringify(products, null, 2));
+console.log("Generated products.json with 1000 items");
